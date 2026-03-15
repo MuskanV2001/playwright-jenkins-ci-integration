9b33955d7f16109d0be743f1e4d81eb39ad3da08
Muskan Vishwakarma <muskanv01lko@gmail.com>
2026-03-15 21:58:56 +0530

    Updated email locator

--- src/pages/loginPage.po.ts
+++ src/pages/loginPage.po.ts
@@ -10,7 +10,7 @@ export class LoginPagePO{
     constructor(private page: Page){
         this.newusersignupheader = page.locator("//h2[contains(text(),'New User Signup')]");
         this.newusername = page.locator("//input[@placeholder='Name']");
-        this.newuseremail = page.locator("//input[@placeholder='Email Address']");
+        this.newuseremail = page.locator("//input[@data-qa='signup-email']");
         this.signupbutton = page.locator("//button[@data-qa='signup-button']")
     }
 }
